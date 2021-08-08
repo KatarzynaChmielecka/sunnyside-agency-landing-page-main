c5619646a3ca110c10706f41ef078f13a5a26326
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2021-08-08 02:43:44 +0200

    Style testimonials section, remove some comments and white spaces

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,20 @@
-"use strict";var myName="asia",logName=function(){console.log("My name is ".concat(myName))};logName(),logName();var test="blaaaa",testa=function(){console.log("Coś tam coś tam ".concat(test))};testa();
+"use strict";
+
+var myName = "asia";
+
+var logName = function logName() {
+  console.log("My name is ".concat(myName));
+};
+
+logName();
+logName();
+"use strict";
+
+var test = 'blaaaa';
+
+var testa = function testa() {
+  console.log("Co\u015B tam co\u015B tam ".concat(test));
+};
+
+testa();
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNjcmlwdHMvaW5kZXguanMiLCJzY3JpcHRzL21haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUNUQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiYXBwLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXCJ1c2Ugc3RyaWN0XCI7XG5cbnZhciBteU5hbWUgPSBcImFzaWFcIjtcblxudmFyIGxvZ05hbWUgPSBmdW5jdGlvbiBsb2dOYW1lKCkge1xuICBjb25zb2xlLmxvZyhcIk15IG5hbWUgaXMgXCIuY29uY2F0KG15TmFtZSkpO1xufTtcblxubG9nTmFtZSgpO1xubG9nTmFtZSgpOyIsIlwidXNlIHN0cmljdFwiO1xuXG52YXIgdGVzdCA9ICdibGFhYWEnO1xuXG52YXIgdGVzdGEgPSBmdW5jdGlvbiB0ZXN0YSgpIHtcbiAgY29uc29sZS5sb2coXCJDb1xcdTAxNUIgdGFtIGNvXFx1MDE1QiB0YW0gXCIuY29uY2F0KHRlc3QpKTtcbn07XG5cbnRlc3RhKCk7Il19
